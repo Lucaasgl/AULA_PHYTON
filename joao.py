b7f540c627163972f2cf799c1fc483c9137b55ab
unknown <lucasmatheusglowienka4@gmail.com>
2023-02-27 22:19:27 -0300

    Atualizacao

---
+++ joao.py
@@ -0,0 +1,4 @@
+print (30 == 30)
+print (type(30 == 31)) 
+print (type("teste"))
+print (11, 1.1, True)
---
+++ lucas.py
@@ -0,0 +1,7 @@
+nome = "Lucas "
+sobrenome = "Matheus Glowienka"
+idade = "18"
+altura = "1.75"
+peso = "80kg"
+
+print ("Meu nome é " +nome +sobrenome+ " Tenho " +idade+ " Anos de Idade, Possuo " +altura+ " De altura")
